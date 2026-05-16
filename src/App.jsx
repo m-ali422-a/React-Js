7e699cfc32f7072429043fdd9078c2fd06602a72
m-ali422-a <mali0004222@gmail.com>
2026-05-16 07:11:32 +0500

    push react folder

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import New from './New'
+
+const App = () => {
+  return (
+    <div>
+      <New />
+    </div>
+  )
+}
+
+export default App
---
+++ src/New.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import img1 from './assets/hero.png'
+
+const New = () => {
+  return (
+    <div>
+      <h1>Muhammad Ali</h1>
+      <img src={img1} alt="Hero" />
+    </div>
+  )
+}
+
+export default New
